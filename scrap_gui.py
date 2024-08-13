1b3c9007dd1a4209bac3eaed9a35356daf17ab88
Pablo Torres <pablotorresalb@gmail.com>
2024-08-13 11:13:04 +0100

    Fixed add term bug

--- scrap_gui.py
+++ scrap_gui.py
@@ -2,17 +2,25 @@
 # libraries Import
 from tkinter import *
 import customtkinter
+from customtkinter import filedialog
+import os
+
+def obtain_subdirs(dir):
+    subdirs = [dir_name for dir_name in os.listdir(dir) if os.path.isdir(os.path.join(dir, dir_name))]
+    return subdirs
 
 class GUI:
 
-  def add_term(self, text=""):
-    curr_row = len(self.term_objects)
+  def add_term(self, text="", curr_row=0):
+    # Adds the entry buttom of the term
     term = customtkinter.CTkEntry(
       master=self.frame_terms,
       placeholder_text="",
       font=("Roboto", 14),
     )
     term.grid(pady=12, padx=10, row=curr_row, column=0)
+    # Adds text to the entry (This only will happen when re-adding all terms at the delete_term
+    # method)
     term.insert(0, text)
     term_delete = customtkinter.CTkButton(
       master=self.frame_terms,
@@ -22,21 +30,39 @@ class GUI:
     )
 
     term_delete.grid(pady=12, padx=10, row=curr_row, column=1)
+
     self.term_objects.append((term, term_delete))
-    print(f"Added at {curr_row}")
 
   def delete_term(self, del_row):
+    print(f"Deleting {self.term_objects[del_row][0].get()}")
     # Remove specific term
     del self.term_objects[del_row]
     # Remove all terms from the grid
     widgets = self.frame_terms.winfo_children()
     for wid in widgets:
       wid.grid_remove()
-    print(self.term_objects)
+    
     # Re-add all terms 
+    # Copy all current terms
     curr_terms = self.term_objects.copy()
+    # Delete list that holds all the terms, because the terms will be added again in
+    # the add_term method.
+    self.term_objects.clear()
+    curr_row = 0
     for term in curr_terms:
-      self.add_term(term[0].get())
+      self.add_term(term[0].get(), curr_row=curr_row)
+      curr_row += 1
+
+  def delete_all_terms(self):
+    for i in range(0, len(self.term_objects)):
+      self.delete_term(i)
+
+  def load_terms_from_dir(self):
+    # Delete all terms
+    self.delete_all_terms()
+    dir = filedialog.askdirectory()
+
+
 
   def __init__(self):
 
@@ -90,13 +116,13 @@ class GUI:
     # Right Panel
 
     self.frame_title = customtkinter.CTkFrame(master=self.frameRight, fg_color="transparent")
-    self.frame_title.pack(pady=5, padx=10, fill="x", expand="True")
+    self.frame_title.pack(pady=5, padx=10, fill="x")
 
     self.frame_terms = customtkinter.CTkScrollableFrame(master=self.frameRight, fg_color="transparent")
     self.frame_terms.pack(pady=5, padx=10, fill="x")
 
-    self.frame_plus_button = customtkinter.CTkFrame(master=self.frameRight, fg_color="transparent")
-    self.frame_plus_button.pack(pady=5, padx=10, fill="x", expand="True")
+    self.frame_buttons = customtkinter.CTkFrame(master=self.frameRight, fg_color="transparent")
+    self.frame_buttons.pack(pady=5, padx=10, fill="x", side="bottom")
 
     Label_id4 = customtkinter.CTkLabel(
         master=self.frame_title,
@@ -107,15 +133,24 @@ class GUI:
         )
     Label_id4.pack(pady=10, padx=10)
 
-
+    
     add_term_button = customtkinter.CTkButton(
-        master=self.frame_plus_button,
+        master=self.frame_buttons,
         text="Add term",
         font=("undefined", 14),
-        command=self.add_term
+        command=lambda: self.add_term(curr_row=len(self.term_objects))
         )
 
     add_term_button.pack(pady=10, padx=10)
+
+    load_from_dir_button = customtkinter.CTkButton(
+        master=self.frame_buttons,
+        text="Load terms from directory",
+        font=("undefined", 14),
+        command=self.load_terms_from_dir
+        )
+
+    load_from_dir_button.pack(pady=10, padx=10)
 
     destination_dir = customtkinter.CTkEntry(
         master=frameDown,
